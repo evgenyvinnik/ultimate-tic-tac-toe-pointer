d3a369ec947067af15484048a82fe6858b4e0da8
Evgeny Vinnik <evgenyvinnik@ya.ru>
2024-03-05 21:44:59 -0800

    downloaded all images from pointerpointer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  async function downloadImage(imageSrc: string, imageName: string) {
+    const image = await fetch(imageSrc);
+    const imageBlog = await image.blob();
+    const imageURL = URL.createObjectURL(imageBlog);
+
+    const link = document.createElement("a");
+    link.href = imageURL;
+    link.download = imageName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  const onClick = () => {
+    for (let i = 0; i < 1000; i++) {
+      let imageName = `${i}.jpg`;
+      downloadImage(
+        `https://pointerpointer.com/images/${imageName}`,
+        imageName
+      );
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={onClick}>Download images</button>
       </header>
     </div>
   );
