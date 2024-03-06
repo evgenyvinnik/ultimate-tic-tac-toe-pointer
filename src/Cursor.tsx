cdd8129d837bff55d0b011bcf785e2ca6ffb90ab
Evgeny Vinnik <evgenyvinnik@ya.ru>
2024-03-06 15:46:28 -0800

    added cursor

--- src/Cursor.tsx
+++ src/Cursor.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Point } from './Point';
+import React from "react";
+import { Point } from "./Point";
 
 export const Cursor: React.FC<{ position: Point }> = ({ position }) => (
   <div
     style={{
       zIndex: 2000,
-      position: 'absolute',
-      transform: `translate(${position.x - 3}px, ${position.y - 4}px)`
+      position: "absolute",
+      transform: `translate(${position.x - 3}px, ${position.y - 4}px)`,
     }}
   >
-    <img src="cursor.png" width="15" alt="pointer" />
+    <img
+      src="./ultimate-tic-tac-toe-pointer/cursor.png"
+      width="15"
+      alt="pointer"
+    />
   </div>
 );
