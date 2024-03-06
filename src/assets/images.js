d82e3f2d2af700f0872a68b96a38192b1600a081
Evgeny Vinnik <evgenyvinnik@ya.ru>
2024-03-05 22:52:35 -0800

    Delete images.js

--- src/assets/images.js
+++
@@ -1,4 +0,0 @@
-export { default as img0 } from './images/0.jpg';
-export { default as img1 } from './images/1.jpg';
-export { default as img2 } from './images/2.jpg';
-export { default as img3 } from './images/3.jpg';
